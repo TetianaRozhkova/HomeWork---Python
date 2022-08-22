0101887b5c4140aaaa0a17368a7abb24476aeb0f
TetianaRozhkova <111787505+TetianaRozhkova@users.noreply.github.com>
2022-08-23 00:50:44 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+def myfunc(a, b, c):
+    if a > b:
+        print("Great!:)")
+    elif a == b:
+        if c > b:
+            print("You got it!:)")
+        else: print("Ohh, no!:(")
+    elif a < b:
+        print("Ohh, No!:(")
+
+
+a = int(input())
+b = int(input())
+c = int(input())
+myfunc(a, b, c)
+
